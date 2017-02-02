80d7e5824c0988b89a7e98b033dbebc56682c9e3
Daniel Elebash <danelebash@hotmail.com>
2017-02-01 23:18:18 -0500

    config

--- src/config/dreamfactoryconfig.js
+++ src/config/dreamfactoryconfig.js
@@ -2,8 +2,8 @@ export default {
   //--------------------------------------------------------------------------
 //  DreamFactory 2.0 instance specific constants
 //--------------------------------------------------------------------------
-  INSTANCE_URL:'https://api.ageektech.com',
-  APP_API_KEY: '1fd07b6f9a10b16602189d794db0abe8a44108d8da59a83a9f5ab46d00132692',
+  INSTANCE_URL:'https://api.test.com',
+  APP_API_KEY: '',
   APP_NAME: 'northwind-app',
   email:'dftester@ageektech.com',
   password: 'test12345',
